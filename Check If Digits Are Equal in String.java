f520e6e8ee95ced531bad91318e2e5374a12d5be
Dhinesh Kumar K <dhineshbala2305@gmail.com>
2026-04-11 11:02:42 +0530

    Add files via upload

---
+++ Check If Digits Are Equal in String.java
@@ -0,0 +1,31 @@
+			Check If Digits Are Equal in String After Operations I
+
+
+class Solution 
+{
+    public boolean hasSameDigits(String s) 
+    {
+        while(s.length()>2)
+        {
+            String temp = "";
+            for(int i=0 ; i<s.length()-1 ; i++)
+            {
+                int one = s.charAt(i) - '0';
+                int two = s.charAt(i+1) - '0';
+
+                int mod = (one+two) % 10;
+
+                temp +=String.valueOf(mod);                
+            }
+
+            s = temp;
+        } 
+
+        if(s.charAt(0) == s.charAt(1))
+        {
+            return true;
+        }
+
+        return false; 
+    }
+}
