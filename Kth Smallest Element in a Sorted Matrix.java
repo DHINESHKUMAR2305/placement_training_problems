c43626ff98c49fa9a895c00cfafb65546de685b1
Dhinesh Kumar K <dhineshbala2305@gmail.com>
2026-05-03 22:55:18 +0530

    Add files via upload

---
+++ Kth Smallest Element in a Sorted Matrix.java
@@ -0,0 +1,21 @@
+				Kth Smallest Element in a Sorted Matrix
+
+
+class Solution 
+{
+    public int kthSmallest(int[][] matrix, int k) 
+    {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i=0 ; i<matrix.length ; i++)
+        {
+            for(int j=0 ; j<matrix[0].length ; j++)
+            {
+                arr.add(matrix[i][j]);
+            }
+        } 
+        Collections.sort(arr);   
+
+        return arr.get(k-1);
+    }
+}
