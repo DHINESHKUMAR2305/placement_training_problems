8a76bcca4cd17866fea1e8a89c9ea6cbc1a427e5
Dhinesh Kumar K <dhineshbala2305@gmail.com>
2026-06-19 21:55:22 +0530

    Add files via upload

---
+++ Permutations.java
@@ -0,0 +1,47 @@
+					Permutations
+
+
+class Solution 
+{
+    public List<List<Integer>> permute(int[] nums) 
+    {
+        List<List<Integer>> xx = new ArrayList<>();
+
+        permutation(nums , 0 , xx);
+
+        return xx;
+
+    }
+    public static void permutation(int[] c , int index , List<List<Integer>> xx)
+    {
+        if(c.length == index)
+        {
+            ArrayList<Integer> pp = new ArrayList<>();
+
+            for(int i :c)
+            {
+                pp.add(i);
+            }
+
+            xx.add(pp);
+            return;
+        }
+
+        for(int i=index ; i<c.length ; i++)
+        {
+            int temp = c[index];
+            c[index] = c[i];
+            c[i] = temp;
+
+            permutation(c , index+1 , xx);
+
+            temp = c[index];
+            c[index] = c[i];
+            c[i] = temp;
+            
+        }
+
+    }
+
+
+}	
