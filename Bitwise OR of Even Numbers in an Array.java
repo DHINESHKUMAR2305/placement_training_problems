ee476b094eaab100955bc40eb94f4df10bf53110
Dhinesh Kumar K <dhineshbala2305@gmail.com>
2026-08-17 08:44:23 +0530

    Add files via upload

---
+++ Bitwise OR of Even Numbers in an Array.java
@@ -0,0 +1,22 @@
+				Bitwise OR of Even Numbers in an Array
+
+
+class Solution 
+{
+    public int evenNumberBitwiseORs(int[] nums) 
+    {
+        int res = 0;
+
+        for(int i : nums)
+        {
+            if(i%2==0)
+            {
+                res = res|i;
+            }
+            
+        }
+
+        return res;
+
+    }
+}
