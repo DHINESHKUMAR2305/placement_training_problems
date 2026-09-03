f1d55d40436a921ea2e1824a6f9688dd03d380b8
Dhinesh Kumar K <dhineshbala2305@gmail.com>
2026-09-03 07:29:45 +0530

    Add files via upload

---
+++ Minimize String Length.java
@@ -0,0 +1,17 @@
+						Minimize String Length
+
+
+class Solution 
+{
+    public int minimizedStringLength(String s) 
+    {
+        HashSet<Character> hs = new HashSet<>();
+
+        for(char c : s.toCharArray())
+        {
+            hs.add(c);
+        }    
+
+        return hs.size();
+    }
+}
